10e662b2dc6028ef676189655940af9c1bc7266b
LakayFTW <MilchmannLetsPlay@gmail.com>
2023-06-09 14:09:52 +0200

    Added option to throw multiple dice

--- src/commands/fun/diceroll.js
+++ src/commands/fun/diceroll.js
@@ -16,23 +16,42 @@ module.exports = {
           { name: "D12", value: "12" },
           { name: "D20", value: "20" }
         )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("How many dices to you want to throw?")
+        .setRequired(true)
     ),
   category: "fun",
   async execute(interaction) {
     const dieOption = interaction.options.getString("die", true);
+    const dieAmount = interaction.options.getString("amount", true);
 
-    var seed = new Date().getTime();
+    var dieArr = [];
     var dieResult = 0;
-    while (dieResult < 1) {
-      dieResult = Math.floor(Math.random(seed) * dieOption);
+    for (var i = 1; i <= dieAmount; i++) {
+      var seed = new Date().getTime();
+      dieResult = Math.floor(Math.random(seed) * dieOption)+1;
+      dieArr.push(dieResult);
+    }
+
+    var dieSequence = "";
+    var allDice = 0;
+    for (var i = 0; i < dieArr.length; i++) {
+      dieSequence += dieArr[i] + ", ";
+      allDice += dieArr[i];
     }
 
     const Build = new EmbedBuilder()
       .setColor(0x0099ff)
       .setTitle("Your throw result:")
-      .addFields({ name: "Result", value: `${dieResult}` })
+      .addFields(
+        { name: "Result", value: `${allDice}` },
+        { name: "Your dice", value: `${dieSequence}` }
+      )
       .setTimestamp()
-      .setFooter({ text: "Provided by Doggo"});
+      .setFooter({ text: "Provided by Doggo" });
     interaction.reply({
       embeds: [Build],
     });
