c80e280764e0032eac766b9d8248176d0a45c55b
LakayFTW <MilchmannLetsPlay@gmail.com>
2023-07-07 23:05:33 +0200

    added error handling to howlong.js and tried to fix howlong package

--- src/commands/fun/howlong.js
+++ src/commands/fun/howlong.js
@@ -17,21 +17,34 @@ module.exports = {
   async execute(interaction) {
     const game = interaction.options.getString("game", true);
 
-    await hltbService.search(game).then((result) => {
+    try {
+      await hltbService.search(game).then((result) => {
+        const Build = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle(`${result[0]['name']}`)
+        .setThumbnail('https://db3pap005files.storage.live.com/y4m2gGcEp8Gv44GNgNZIXStkKJFl27dMfQEJSqyosWwM0mjUAX-zB4vmOXZe8d5fM5jysbit6fu1JgM8CKuEtEOvkmjN1eoBmNYUtE9CsLyNUrTVCeg5jCcuXTFp8NO6jiHtwc6yyfg1NBYppDZXJ_Xxzf1a9GQu21j4TBe7CQLhNVwo2AwAO4JRiZXmAsurOM-PQFkmxb5wtSqfxpKbAYuB_6NcjYkME5veypBs-0brJw?encodeFailures=1&width=64&height=65')
+        .addFields(
+          { name: "Main Story", value: `${result[0]['gameplayMain']} Hours`, inline: true },
+          { name: "Main + Extra", value: `${result[0]['gameplayMainExtra']} Hours`, inline: true },
+          { name: "Completionist", value: `${result[0]['gameplayCompletionist']} Hours`, inline: true}
+        )
+        .setImage(`${result[0]['imageUrl']}`);
+        interaction.reply({
+          // content: `Game: ${result[0]["name"]}\nMain Story: ${result[0]['gameplayMain']} Std\nMain + Extra: ${result[0]['gameplayMainExtra']} Std\nCompletionist: ${result[0]['gameplayCompletionist']} Std\n${result[0]['imageUrl']}`,
+          embeds: [Build],
+        });
+      });
+    } catch {
       const Build = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setTitle(`${result[0]['name']}`)
-      .setThumbnail('https://db3pap005files.storage.live.com/y4m2gGcEp8Gv44GNgNZIXStkKJFl27dMfQEJSqyosWwM0mjUAX-zB4vmOXZe8d5fM5jysbit6fu1JgM8CKuEtEOvkmjN1eoBmNYUtE9CsLyNUrTVCeg5jCcuXTFp8NO6jiHtwc6yyfg1NBYppDZXJ_Xxzf1a9GQu21j4TBe7CQLhNVwo2AwAO4JRiZXmAsurOM-PQFkmxb5wtSqfxpKbAYuB_6NcjYkME5veypBs-0brJw?encodeFailures=1&width=64&height=65')
+      .setColor(0xC50000)
+      .setTitle("Not Responding")
+      .setThumbnail("https://db3pap005files.storage.live.com/y4m2gGcEp8Gv44GNgNZIXStkKJFl27dMfQEJSqyosWwM0mjUAX-zB4vmOXZe8d5fM5jysbit6fu1JgM8CKuEtEOvkmjN1eoBmNYUtE9CsLyNUrTVCeg5jCcuXTFp8NO6jiHtwc6yyfg1NBYppDZXJ_Xxzf1a9GQu21j4TBe7CQLhNVwo2AwAO4JRiZXmAsurOM-PQFkmxb5wtSqfxpKbAYuB_6NcjYkME5veypBs-0brJw?encodeFailures=1&width=64&height=65")
       .addFields(
-        { name: "Main Story", value: `${result[0]['gameplayMain']} Hours`, inline: true },
-        { name: "Main + Extra", value: `${result[0]['gameplayMainExtra']} Hours`, inline: true },
-        { name: "Completionist", value: `${result[0]['gameplayCompletionist']} Hours`, inline: true}
-      )
-      .setImage(`${result[0]['imageUrl']}`);
+        { name: "ERROR", value: "The request could not be resolved\n We are working on a solution!"}
+      );
       interaction.reply({
-        // content: `Game: ${result[0]["name"]}\nMain Story: ${result[0]['gameplayMain']} Std\nMain + Extra: ${result[0]['gameplayMainExtra']} Std\nCompletionist: ${result[0]['gameplayCompletionist']} Std\n${result[0]['imageUrl']}`,
         embeds: [Build],
       });
-    });
+    };
   },
 };
