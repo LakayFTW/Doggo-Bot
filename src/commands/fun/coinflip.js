98e7dc182f1c5443c309609332a4e8c1f0617816
LakayFTW <67958199+LakayFTW@users.noreply.github.com>
2023-07-10 21:52:39 +0200

    2 add buttons to commands (#3)
    * added retry buttons to coinflip and diceroll, only works once atm
    
    * Added emote to diceroll; Fixed issue where comma would appear at only one dice
    
    * Added loop to enable unlimited retries each within 60 seconds

--- src/commands/fun/coinflip.js
+++ src/commands/fun/coinflip.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,21 +12,65 @@ module.exports = {
     .setDescription("Flips a coin"),
   category: "fun",
   async execute(interaction) {
-    var seed = new Date().getTime();
-    var flip = Math.floor(Math.random(seed) * 2);
+    const retry = new ButtonBuilder()
+      .setCustomId("retry")
+      .setLabel("Retry")
+      .setStyle(ButtonStyle.Success);
+    var retries = 0;
 
-    const Build = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("Result")
-      .addFields({ name: "Coinside", value: `${getCoinSide(flip)}` })
-      .setTimestamp()
-      .setFooter({ text: "Provided by Doggo" });
+    const row = new ActionRowBuilder().addComponents(retry);
+    var Build = await buildEmbed(retries);
 
-    interaction.reply({
+    const response = await interaction.reply({
       embeds: [Build],
+      components: [row],
     });
+
+    const collectorFilter = (i) => i.user.id === interaction.user.id;
+    while (true) {
+      try {
+        var confirmation = await response.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 60000,
+        });
+        if (confirmation.customId === "retry") {
+          retries++;
+          Build = await buildEmbed(retries);
+          await confirmation.update({
+            content: "Your retry",
+            embeds: [Build],
+            components: [row],
+          });
+          continue;
+        }
+      } catch (e) {
+        await interaction.editReply({
+          content: "Confirmation not received within 1 minute, cancelling",
+          components: [],
+        });
+        break;
+      }
+    }
   },
 };
+
+async function buildEmbed(retries) {
+  var Build = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("Result")
+    .addFields(
+      { name: "Coinside", value: `${getCoinSide(flip())}`, inline: true },
+      { name: "Retries", value: `${retries}`, inline: true },
+    )
+    .setTimestamp()
+    .setFooter({ text: "Provided by Doggo" });
+  return Build;
+}
+
+function flip() {
+  var seed = new Date().getTime();
+  return Math.floor(Math.random(seed) * 2);
+}
 
 function getCoinSide(flipResult) {
   if (flipResult === 0) return "Heads";
--- src/commands/fun/diceroll.js
+++ src/commands/fun/diceroll.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,32 +34,85 @@ module.exports = {
     const dieOption = interaction.options.getString("die", true);
     const dieAmount = interaction.options.getString("amount", true);
 
-    var dieArr = [];
-    var dieResult = 0;
-    for (var i = 1; i <= dieAmount; i++) {
-      var seed = new Date().getTime();
-      dieResult = Math.floor(Math.random(seed) * dieOption)+1;
-      dieArr.push(dieResult);
-    }
+    const retry = new ButtonBuilder()
+      .setCustomId("retry")
+      .setLabel("Retry")
+      .setStyle(ButtonStyle.Success);
 
-    var dieSequence = "";
-    var allDice = 0;
-    for (var i = 0; i < dieArr.length; i++) {
-      dieSequence += dieArr[i] + ", ";
-      allDice += dieArr[i];
-    }
+    var retries = 0;
+    const row = new ActionRowBuilder().addComponents(retry);
+    var result = await generateDie(dieOption, dieAmount);
+    var Build = await buildEmbed(result, retries);
 
-    const Build = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("Your throw result:")
-      .addFields(
-        { name: "Result", value: `${allDice}` },
-        { name: "Your dice", value: `${dieSequence}` }
-      )
-      .setTimestamp()
-      .setFooter({ text: "Provided by Doggo" });
-    interaction.reply({
+    const response = await interaction.reply({
       embeds: [Build],
+      components: [row],
     });
+
+    const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+    while (true) {
+      try {
+        const confirmation = await response.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 60000,
+        });
+
+        if (confirmation.customId === "retry") {
+          retries++;
+          result = await generateDie(dieOption, dieAmount);
+          Build = await buildEmbed(result, retries);
+          await confirmation.update({
+            content: "Your retry",
+            embeds: [Build],
+            components: [row],
+          });
+          continue;
+        }
+      } catch (e) {
+        await interaction.editReply({
+          content: "Confirmation not received within 1 minute, cancelling",
+          components: [],
+        });
+        break;
+      }
+    }
   },
 };
+
+async function generateDie(dieOption, dieAmount) {
+  var result = [];
+  var dieArr = [];
+  var dieResult = 0;
+  for (var i = 1; i <= dieAmount; i++) {
+    var seed = new Date().getTime();
+    dieResult = Math.floor(Math.random(seed) * dieOption) + 1;
+    dieArr.push(dieResult);
+  }
+
+  var dieSequence = "";
+  var allDice = 0;
+  for (var i = 0; i < dieArr.length; i++) {
+    dieSequence += dieArr[i];
+    dieSequence = +", ";
+    allDice += dieArr[i];
+  }
+  result.push(allDice);
+  result.push(dieArr);
+  return result;
+}
+
+async function buildEmbed(result, retries) {
+  var Build = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("Your throw result: :game_die:")
+    .addFields(
+      { name: "Result", value: `${result[0]}`, inline: true },
+      { name: "Retries", value: `${retries}`, inline: true },
+      { name: "Your dice", value: `${result[1]}` }
+    )
+    .setTimestamp()
+    .setFooter({ text: "Provided by Doggo" });
+
+  return Build;
+}
