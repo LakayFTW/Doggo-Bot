15c33de0af80b587a9198764b88be05761ed02bf
LakayFTW <MilchmannLetsPlay@gmail.com>
2023-07-10 20:04:30 +0200

    added retry buttons to coinflip and diceroll, only works once atm

--- src/commands/fun/coinflip.js
+++ src/commands/fun/coinflip.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,21 +12,57 @@ module.exports = {
     .setDescription("Flips a coin"),
   category: "fun",
   async execute(interaction) {
-    var seed = new Date().getTime();
-    var flip = Math.floor(Math.random(seed) * 2);
+    const retry = new ButtonBuilder()
+      .setCustomId("retry")
+      .setLabel("Retry")
+      .setStyle(ButtonStyle.Success);
 
-    const Build = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("Result")
-      .addFields({ name: "Coinside", value: `${getCoinSide(flip)}` })
-      .setTimestamp()
-      .setFooter({ text: "Provided by Doggo" });
+    const row = new ActionRowBuilder().addComponents(retry);
+    var Build = await buildEmbed();
 
-    interaction.reply({
+    const response = await interaction.reply({
       embeds: [Build],
+      components: [row],
     });
+
+    const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+    try {
+      const confirmation = await response.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60000,
+      });
+
+      if (confirmation.customId === "retry") {
+        await confirmation.update({
+          content: "Your retry",
+          embeds: [Build],
+          components: [],
+        });
+      }
+    } catch (e) {
+      await interaction.editReply({
+        content: "Confirmation not received within 1 minute, canelling",
+        components: [],
+      });
+    }
   },
 };
+
+async function buildEmbed() {
+  var Build = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("Result")
+    .addFields({ name: "Coinside", value: `${getCoinSide(flip())}` })
+    .setTimestamp()
+    .setFooter({ text: "Provided by Doggo" });
+  return Build;
+}
+
+function flip() {
+  var seed = new Date().getTime();
+  return Math.floor(Math.random(seed) * 2);
+}
 
 function getCoinSide(flipResult) {
   if (flipResult === 0) return "Heads";
