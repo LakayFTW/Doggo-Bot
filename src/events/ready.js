fd2e38a4193b7b077692d29f793acb4a42ee0b72
LakayFTW <MilchmannLetsPlay@gmail.com>
2024-05-09 20:24:59 +0200

    Added activity

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,19 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: `Fetch`,
+          details: `with his human`,
+          type: ActivityType.Playing,
+        },
+      ],
+    });
   },
 };
